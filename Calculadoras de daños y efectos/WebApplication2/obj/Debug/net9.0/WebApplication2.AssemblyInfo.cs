bdc2d2f83272f8634b94c6408c526cb73f558fbb
Rogerelzorro <rogercollv@gmail.com>
2026-02-16 09:57:57 +0100

    1

--- Calculadoras de daños y efectos/WebApplication2/obj/Debug/net9.0/WebApplication2.AssemblyInfo.cs
+++ Calculadoras de daños y efectos/WebApplication2/obj/Debug/net9.0/WebApplication2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60553f3dcb4c88f4627d16781876943c11b360b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd2366f20b4c098172fbc9cec6620e7c0b460e6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
